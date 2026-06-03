6337698a817e49dac882a4c9394d6d8b39214312
mariyam-hanna <havvamariyam2001@gmail.com>
2026-06-03 11:59:10 +0530

    Added routing and MediaScribe AI UI updates

---
+++ frontend/GenAIAPP.UI/src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+  constructor(private router: Router) { }
+
+  navigateToUpload() {
+    this.router.navigate(['/upload']);
+  }
+}
